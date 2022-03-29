6972f6096052427d74684d15a46b429965047f65
Amr Rommaneh <amrrommaneh@ip-192-168-2-35.ec2.internal>
2022-03-29 10:11:03 +0300

    second

--- build.sbt
+++ build.sbt
@@ -1,13 +1,12 @@
- version := "0.1.0-SNAPSHOT"
+version := "0.1.0-SNAPSHOT"
 
- scalaVersion := "2.13.8"
+scalaVersion := "2.13.8"
 
 name := "amrTraining_sparkTask"
 val sparkVersion = "3.2.1"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "3.2.1",
-  "org.apache.spark" %% "spark-sql" % "3.2.1"
+  "org.apache.spark" %% "spark-core" % "3.2.1" % "provided",
+  "org.apache.spark" %% "spark-sql" % "3.2.1" % "provided"
 )
-libraryDependencies += "org.apache.spark" %% "spark-hive" % sparkVersion
-libraryDependencies += "org.postgresql" % "postgresql" % "42.2.12"
+libraryDependencies += "org.apache.spark" %% "spark-hive" % sparkVersion % "provided"
--- src/main/scala/com/amrTraining/spark/SparkTask.scala
+++ src/main/scala/com/amrTraining/spark/SparkTask.scala
@@ -1,4 +1,6 @@
 package com.amrTraining.spark
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3Client
 import org.apache.derby.iapi.types.Like
 import org.apache.spark._
 import org.apache.spark.SparkContext._
@@ -12,32 +14,48 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.storage.StorageLevel
 
 object SparkTask {
-  val spark =
-    SparkSession
-      .builder
-      .appName("Car Theft App")
-      .enableHiveSupport()
-      .getOrCreate()
 
-  val sqlContext = new org.apache.spark.sql.hive.HiveContext(spark.sparkContext)
 
 
   def main (args : Array[String]) :Unit ={
 
+
+//    val yourAWSCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+//    val amazonS3Client = new AmazonS3Client(yourAWSCredentials)
+//    val bucketName = "modelCountry"          // specifying bucket name
+//    val bucketName1 = "topFiveCountries"          // specifying bucket name
+//    val bucketName2 = "TopFiveModels"          // specifying bucket name
+//    val bucketName3 = "TopFiveStates"          // specifying bucket name
+
+
+    val spark =
+         SparkSession
+        .builder
+        .appName("Car Theft App")
+        .enableHiveSupport()
+        .getOrCreate()
+
+    val sqlContext = new org.apache.spark.sql.hive.HiveContext(spark.sparkContext)
+
     var cars = spark.read.option("header","true").csv("s3://dev-harri-reporting/amr.rommaneh/cars.csv")
 
-    cars=cars.withColumnRenamed("Car Brand","car_brand")
-    cars= cars.withColumnRenamed("Country of Origin","country_of_origin")
+    cars = cars.withColumnRenamed("Car Brand","car_brand")
+    cars = cars.withColumnRenamed("Country of Origin","country_of_origin")
 
     cars.cache()
     cars.persist(StorageLevel.MEMORY_AND_DISK)
 
+    cars.show()
+
     var df = spark.read.option("header","true").csv("s3://dev-harri-reporting/amr.rommaneh/2015_State_Top10Report_wTotalThefts.csv")
-    df=  df.withColumnRenamed("Make/Model","make_model")
+    df =  df.withColumnRenamed("Make/Model","make_model")
+
+
 
     df.createOrReplaceTempView("thefts")
     cars.createOrReplaceTempView("cars")
 
+    df.show()
 
     var joined =spark.sql("select * FROM thefts left join cars on thefts.make_model like concat (cars.car_brand,'%')")
     joined = joined.withColumn("model",regexp_replace(col("make_model"), col("car_brand"), lit("")))
@@ -47,87 +65,59 @@ object SparkTask {
     joined.cache()
     joined.persist(StorageLevel.MEMORY_AND_DISK)
 
+    joined.show()
 
+
+    // TODO: Internal tables VS external tables
     sqlContext.sql("create database car_theft")
     sqlContext.sql("use car_theft")
     sqlContext.sql("CREATE TABLE IF NOT EXISTS Cars(car_brand STRING, country_of_origin STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
     sqlContext.sql("CREATE TABLE IF NOT EXISTS Thefts(state STRING,rank int, make_model STRING,model_year int,thefts int) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
-    cars.write.saveAsTable("carthreft.Cars")
-    df.write.saveAsTable("carthreft.Thefts")
+    sqlContext.sql("CREATE TABLE IF NOT EXISTS thefts_with_origin(state STRING,rank int, car_brand STRING,model STRING,country_of_origin STRING,model_year int,thefts int) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
+
+
+    cars.write.option("header","true").mode("overwrite").saveAsTable("carthreft.Cars")
+    df.write.option("header","true").mode("overwrite").saveAsTable("carthreft.Thefts")
 
 
     joined.write.option("header","true").mode("overwrite").saveAsTable("carthreft.thefts_with_origin")
 
     joined.write.option("header","true").partitionBy("rank").mode("overwrite").saveAsTable("carthreft.rank")
 
+    sqlContext.sql("select * FROM thefts_with_origin").show(false)
 
 
     var modelCountry=sqlContext.sql("Select model,country_of_origin from  thefts_with_origin")
-    var joinedTable=sqlContext.sql("Select * from Cars INNER Join Thefts On Cars.car_brand=Thefts.make ")
 
+    modelCountry.show()
     joined.cache()
     joined.persist(StorageLevel.MEMORY_AND_DISK)
 
 
-    println( " enter your choice ")
-  println("1) get the model with country of origin")
-  println("2) top 5 countries their cars are stolen")
-  println("3) update the records")
-  println("4) top 5 states their cars are stolen")
-  println("5) top 5 models  are stolen")
 
-  var choice = scala.io.StdIn.readInt()
-
-  if(choice == 1){
-
-    modelCountry.show()
-
-  }else if (choice ==2){
-    getTopFiveCountries()
-  }else if (choice ==3){
-    println("enter the path of the csv file of the new records")
-    var path = scala.io.StdIn.readLine()
-
-    update(path)
-  }else if (choice==4){
-    getTopFiveStates()
-  }else if (choice==5){
-    getTopFiveModels()
-  }else{
-    println("wrong choice")
-  }
-
-
-
-}
-
-
-  def update(path :String): Unit ={
     sqlContext.sql("DROP TABLE new_thefts;")
-    var df = spark.read.option("header","true").csv(path)
-    df =    df.withColumnRenamed("Make/Model","make_model")
+    var newThefts = spark.read.option("header","true").csv(args(0))
+    newThefts =    newThefts.withColumnRenamed("Make/Model","make_model")
 
- df.createOrReplaceTempView("new_thefts")
+    newThefts.createOrReplaceTempView("new_thefts")
 
-    var newThefts =   sqlContext.sql("select * FROM new_thefts left join Cars on new_thefts.make_model like concat (Cars.car_brand,'%')")
-    newThefts = newThefts.withColumn("model",regexp_replace(col("make_model"), col("car_brand"), lit("")))
-
-
-   // sqlContext.sql("CREATE TABLE IF NOT EXISTS new_thefts(state STRING,rank int, make_model STRING,model_year int,thefts int) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
+    var newThefts_ =   sqlContext.sql("select * FROM new_thefts left join Cars on new_thefts.make_model like concat (Cars.car_brand,'%')")
+    newThefts_ = newThefts_.withColumn("model",regexp_replace(col("make_model"), col("car_brand"), lit("")))
+    sqlContext.sql("CREATE TABLE IF NOT EXISTS new_thefts(state STRING,rank int, car_brand STRING,model STRING,country_of_origin STRING,model_year int,thefts int) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
 
 
-    var x=sqlContext.sql("Select * from thefts_with_origin ")
-    newThefts.write.option("header","true").mode("overwrite").saveAsTable("carthreft.new_thefts")
+
+
+    newThefts_.write.option("header","true").mode("overwrite").saveAsTable("carthreft.new_thefts")
 
 
     var joinedTable=sqlContext.sql("Update  thefts_with_origin set thefts_with_origin.thefts=thefts_with_origin.thefts+new_thefts.thefts " +
       "Join new_thefts On thefts_with_origin.model=new_thefts.model And thefts_with_origin.model_year=new_thefts.model_year And thefts_with_origin.car_brand=new_thefts.car_brand ")
+    joinedTable.write.option("header","true").mode("overwrite").saveAsTable("carthreft.thefts_with_origin")
+
+    joinedTable.show()
 
 
-  }
-
-  def getTopFiveCountries ():Unit={
-    //Extract a csv file contains the most 5 countries from where Americans buy their thefted cars ?
 
     var topFiveCountries =sqlContext.sql("SELECT TOP 5 * FROM " +
       " (Select Sum(thefts_with_origin.thefts),thefts_with_origin.country_of_origin " +
@@ -137,10 +127,7 @@ object SparkTask {
 
     topFiveCountries.write.csv("topFiveCountries")
     topFiveCountries.show()
-  }
 
-
-  def getTopFiveModels():Unit={
 
     val TopFiveModels =sqlContext.sql("SELECT TOP 5 * FROM " +
       " (Select Sum(thefts_with_origin.thefts),DISTINCT thefts_with_origin.make,thefts_with_origin.model " +
@@ -149,11 +136,8 @@ object SparkTask {
       "ORDER BY Sum(thefts_with_origin.thefts) ASC)")
 
     TopFiveModels.write.csv("TopFiveModels")
+    TopFiveModels.show()
 
-  }
-
-
-  def getTopFiveStates():Unit={
 
     val TopFiveStates =sqlContext.sql("SELECT TOP 5 * FROM " +
       " (Select Sum(thefts_with_origin.thefts),thefts_with_origin.state " +
@@ -162,6 +146,67 @@ object SparkTask {
       "ORDER BY Sum(thefts_with_origin.thefts) ASC)")
 
     TopFiveStates.write.csv("TopFiveStates")
+    TopFiveStates.show()
+
+
+    //
+//
+//    println( " enter your choice ")
+//  println("1) get the model with country of origin")
+//  println("2) top 5 countries their cars are stolen")
+//  println("3) update the records")
+//  println("4) top 5 states their cars are stolen")
+//  println("5) top 5 models  are stolen")
+//
+//  var choice = scala.io.StdIn.readInt()
+//
+//  if(choice == 1){
+//
+//    modelCountry.show()
+//
+//  }else if (choice ==2){
+//    getTopFiveCountries()
+//  }else if (choice ==3){
+//    println("enter the path of the csv file of the new records")
+//    var path = scala.io.StdIn.readLine()
+//
+//    update(path)
+//  }else if (choice==4){
+//    getTopFiveStates()
+//  }else if (choice==5){
+//    getTopFiveModels()
+//  }else{
+//    println("wrong choice")
+//  }
+//
+
+
+}
+
+
+  def update(path :String): Unit ={
+  }
+
+  def getTopFiveCountries ():Unit={
+    //Extract a csv file contains the most 5 countries from where Americans buy their thefted cars ?
+  }
+
+
+  def getTopFiveModels():Unit={
+
+
+  }
+
+
+  def getTopFiveStates():Unit={
+//
+//    val TopFiveStates =sqlContext.sql("SELECT TOP 5 * FROM " +
+//      " (Select Sum(thefts_with_origin.thefts),thefts_with_origin.state " +
+//      "from thefts_with_origin " +
+//      "GroupBy state " +
+//      "ORDER BY Sum(thefts_with_origin.thefts) ASC)")
+//
+//    TopFiveStates.write.csv("TopFiveStates")
 
   }
 
